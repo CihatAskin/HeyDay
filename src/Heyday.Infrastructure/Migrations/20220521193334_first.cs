a4b228f508546f6d86ff6890a45e0781a7a30c4c
cihataskin <canali450@gmail>
2022-05-24 08:54:22 +0300

    migration refactoring

---
+++ src/Heyday.Infrastructure/Migrations/20220521193334_first.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Heyday.Infrastructure.Migrations
+{
+    public partial class first : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "schedule",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false),
+                    notes = table.Column<string>(type: "text", nullable: true),
+                    date_time = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    suitable_hours = table.Column<string>(type: "jsonb", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_schedule", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "schedule");
+        }
+    }
+}
