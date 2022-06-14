635fce718afaf53e2e86d10cd91519b945134ba9
cihat askin <canali450@gmail.com>
2022-06-14 23:15:52 +0300

    added tableContext

--- src/Heyday.Shared/Schedule/CreateScheduleRequest.cs
+++ src/Heyday.Shared/Schedule/CreateScheduleRequest.cs
@@ -2,10 +2,11 @@
 namespace Heyday.Shared.Schedule;
 public class CreateScheduleRequest
 {
-    public string Title { get; set; }
-    public Guid ManagerId { get; set; }
+    public string Title { get; set; }   
+    public string Description { get; set; }   
     public TimeSpan Period { get; set; }
     public DateTime StartDate { get; set; }
+    public Guid ManagerId { get; set; }
     public List<short> HourKeys { get; set; }
     public List<Guid> UserIds { get; set; }
 }
